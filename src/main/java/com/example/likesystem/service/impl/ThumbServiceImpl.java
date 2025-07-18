2785542ccfc789292e21f6e333c012f05e29862e
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:35 +0800

    feat: 实现 ThumbService 接口，继承 ServiceImpl

---
+++ src/main/java/com/example/likesystem/service/impl/ThumbServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.likesystem.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.likesystem.model.entity.Thumb;
+import com.example.likesystem.service.ThumbService;
+import com.example.likesystem.mapper.ThumbMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author alanz
+*/
+@Service
+public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb>
+    implements ThumbService{
+
+}
+
+
+
+
