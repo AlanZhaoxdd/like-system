97f1fa0af0afe522b17cfa94cf202b619e7955ee
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:18 +0800

    feat: 添加 ThumbMapper 接口，继承 MyBatis-Plus BaseMapper

---
+++ src/main/java/com/example/likesystem/mapper/ThumbMapper.java
@@ -0,0 +1,17 @@
+package com.example.likesystem.mapper;
+
+import com.example.likesystem.model.entity.Thumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author alanz
+*/
+@Mapper
+public interface ThumbMapper extends BaseMapper<Thumb> {
+
+}
+
+
+
+
