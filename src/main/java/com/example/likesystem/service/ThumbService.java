71dba80b850f171726997b1ff717b74c270e350f
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:31 +0800

    feat: 添加 ThumbService 接口

---
+++ src/main/java/com/example/likesystem/service/ThumbService.java
@@ -0,0 +1,11 @@
+package com.example.likesystem.service;
+
+import com.example.likesystem.model.entity.Thumb;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author alanz
+*/
+public interface ThumbService extends IService<Thumb> {
+
+}
