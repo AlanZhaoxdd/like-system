319410ef1594d4503bff24c42e92368a032bfc47
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:30 +0800

    feat: 添加 BlogService 接口

---
+++ src/main/java/com/example/likesystem/service/BlogService.java
@@ -0,0 +1,11 @@
+package com.example.likesystem.service;
+
+import com.example.likesystem.model.entity.Blog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author alanz
+*/
+public interface BlogService extends IService<Blog> {
+
+}
