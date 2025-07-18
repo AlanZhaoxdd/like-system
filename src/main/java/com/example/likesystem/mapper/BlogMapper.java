e87550421443bf2a69ee6f3dd3d9bd6d28cdcf61
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:17 +0800

    feat: 添加 BlogMapper 接口，继承 MyBatis-Plus BaseMapper

---
+++ src/main/java/com/example/likesystem/mapper/BlogMapper.java
@@ -0,0 +1,17 @@
+package com.example.likesystem.mapper;
+
+import com.example.likesystem.model.entity.Blog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author alanz
+*/
+@Mapper
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
+
+
+
+
