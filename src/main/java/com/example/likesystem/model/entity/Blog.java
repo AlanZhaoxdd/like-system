2b90c410d9a0e8d15e7bf8bc281fcf8ab958bff4
AlanZhao <alan.zhao@example.com>
2025-07-18 21:04:22 +0800

    feat: 添加 Blog 实体类，定义博客字段与表结构映射

---
+++ src/main/java/com/example/likesystem/model/entity/Blog.java
@@ -0,0 +1,50 @@
+package com.example.likesystem.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName blog
+ */
+@TableName(value ="blog")
+@Data
+public class Blog implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 封面
+     */
+    private String coverImg;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 点赞数
+     */
+    private Integer thumbCount;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
