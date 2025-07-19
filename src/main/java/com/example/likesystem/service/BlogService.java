ba560bcda469f11b2e3b637579b30c7cbed771e5
AlanZhao <alan.zhao@example.com>
2025-07-19 18:36:46 +0800

    修改 BlogService 接口，增加获取带点赞状态 BlogVO 的方法

--- src/main/java/com/example/likesystem/service/BlogService.java
+++ src/main/java/com/example/likesystem/service/BlogService.java
@@ -2,10 +2,20 @@ package com.example.likesystem.service;
 
 import com.example.likesystem.model.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.likesystem.model.vo.BlogVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 /**
 * @author alanz
 */
 public interface BlogService extends IService<Blog> {
+
+    BlogVO getBlogVOById(long blogId, HttpServletRequest request);
+
+    List<BlogVO> getBlogVOList(List<Blog> blogList, HttpServletRequest request);
+
+
 
 }
