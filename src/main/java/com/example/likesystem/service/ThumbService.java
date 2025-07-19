d3eb46ceed96baa80978465244c9552cb9b0068a
AlanZhao <alan.zhao@example.com>
2025-07-19 18:36:18 +0800

    新增 ThumbService 接口，定义点赞和取消点赞方法

--- src/main/java/com/example/likesystem/service/ThumbService.java
+++ src/main/java/com/example/likesystem/service/ThumbService.java
@@ -1,11 +1,29 @@
 package com.example.likesystem.service;
 
+import com.example.likesystem.model.dto.thumb.DoThumbRequest;
 import com.example.likesystem.model.entity.Thumb;
 import com.baomidou.mybatisplus.extension.service.IService;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
 * @author alanz
 */
 public interface ThumbService extends IService<Thumb> {
+
+    /**
+     * 点赞
+     * @param doThumbRequest
+     * @param request
+     * @return {@link Boolean}
+     */
+    Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request);
+
+    /**
+     * 取消点赞
+     * @param doThumbRequest
+     * @param request
+     * @return {@link Boolean}
+     */
+    Boolean undoThumb(DoThumbRequest doThumbRequest, HttpServletRequest request);
 
 }
