f2bf39ab5fe85e86937e1d4cdf6310ce31c66638
AlanZhao <alan.zhao@example.com>
2025-07-21 18:00:48 +0800

    feat: 添加 Blog 实体类，包含标题、内容、作者、点赞数等字段

--- src/main/java/com/example/likesystem/model/entity/Blog.java
+++ src/main/java/com/example/likesystem/model/entity/Blog.java
@@ -5,17 +5,19 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
+import java.util.Date;
+
 import lombok.Data;
 
 /**
- * 
+ *
  * @TableName blog
  */
 @TableName(value ="blog")
 @Data
 public class Blog implements Serializable {
     /**
-     * 
+     *
      */
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
@@ -44,6 +46,16 @@ public class Blog implements Serializable {
      * 点赞数
      */
     private Integer thumbCount;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
