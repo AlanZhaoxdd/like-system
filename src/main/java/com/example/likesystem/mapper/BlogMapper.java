b7efa02ef343524ec26ae691cadd332bb4b0f45b
AlanZhao <alan.zhao@example.com>
2025-07-26 00:16:59 +0800

    feat(blog): 增加 batchUpdateThumbCount 方法用于批量更新点赞数

--- src/main/java/com/example/likesystem/mapper/BlogMapper.java
+++ src/main/java/com/example/likesystem/mapper/BlogMapper.java
@@ -3,13 +3,16 @@ package com.example.likesystem.mapper;
 import com.example.likesystem.model.entity.Blog;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
 * @author alanz
 */
 @Mapper
 public interface BlogMapper extends BaseMapper<Blog> {
-
+    void batchUpdateThumbCount(@Param("countMap") Map<Long, Long> countMap);
 }
 
 
